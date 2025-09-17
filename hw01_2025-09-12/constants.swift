6191e2241080afebe57333d2c61c32457fec6904
VDovydenkov <dov@mlbox.ru>
2025-09-17 13:30:42 +0300

    Добавлено решение первой задачи: объявление констант, конкатенация и вывод результата.

---
+++ hw01_2025-09-12/constants.swift
@@ -0,0 +1,13 @@
+// Объявляем константу firstString со значением "I can"
+let firstString = "I can"
+
+// Объявляем константу secondString со значением "code"
+let secondString = "code"
+
+// Теперь нам нужно вывести на консоль фразу "I can code!".
+// Для этого объединим значения констант с пробелом между ними
+// и добавим восклицательный знак в конце.
+let result = firstString + " " + secondString + "!"
+
+// Выводим получившуюся строку на консоль
+print(result)
